96425221465436cf18a5610a14d4606322605afd
Sebastian Bauersfeld <>
2022-08-11 18:39:10 +0700

    update

--- java/unit-tests/taint_through_crudrepository_save/Test.java
+++ java/unit-tests/taint_through_crudrepository_save/Test.java
@@ -1,21 +1,17 @@
-package test.cwe079.cwe.examples;
-
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.data.repository.CrudRepository;
 
 public class Test extends HttpServlet {
+	public static CrudRepository<String, String> cr = null;
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException {
-		response.getWriter().print(
-			"a" + request.getParameter("page")
-		);
-
-		response.getWriter().print(
-			"a" + request.getParameter("page").replace("/", "")
-		);
+		String taint = request.getParameter("page");
+		taint = cr.save(taint);
+		response.getWriter().print("a " + taint);
 	}
 }
